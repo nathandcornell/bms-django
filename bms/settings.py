9e585f0dc0d511f8833209dfa2711c41a1daeb0c
Nate Cornell <nathandcornell@gmail.com>
2018-05-28 05:20:23 +0000

    Replace service_objects with django_extensions. Add pyserial

--- bms/settings.py
+++ bms/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'service_objects',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
