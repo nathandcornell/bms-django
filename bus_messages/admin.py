ecd23f70d7a526e83addaa067dc91fa686e4d831
Nate Cornell <nathandcornell@gmail.com>
2018-05-29 21:21:51 -0700

    Add bus messages to admin

---
+++ bus_messages/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import ModuleMsg
+from .models import StringMsg
+
+# Register your models here.
+admin.site.register(ModuleMsg)
+admin.site.register(StringMsg)
