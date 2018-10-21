4c2e725d4be34b025522fb4d2b4dbc1dfcb7d3f1
Nate Cornell <nathandcornell@gmail.com>
2018-10-21 13:39:33 -0700

    Move gauges out to a separate component

---
+++ bus_messages/static/js/components/Container/Module/GaugeSet/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import ReactSpeedometer from "react-d3-speedometer"
+
+export default class GuageSet extends React.Component {
+  render () {
+    const { classes, data } = this.props
+
+    return (
+      <Paper className={classes.moduleSection}>
+        <Grid container item xs={12} spacing={16}>
+          <Grid item xs={4}>
+            <ReactSpeedometer
+              width={120}
+              height={90}
+              ringWidth={20}
+              minValue={0}
+              maxValue={50}
+              startColor="blue"
+              endColor="purple"
+              value={data.avg_cell_temperature}
+              currentValueText="Temp: ${value}º C"
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <ReactSpeedometer
+              width={120}
+              height={90}
+              ringWidth={20}
+              minValue={1.8}
+              maxValue={6.0}
+              value={data.avg_cell_voltage / 1000}
+              currentValueText="Voltage: ${value} V"
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <ReactSpeedometer
+              width={120}
+              height={90}
+              ringWidth={20}
+              maxValue={10.0}
+              value={data.amperes}
+              currentValueText="Amperes: ${value}"
+            />
+          </Grid>
+        </Grid>
+      </Paper>
+    )
+  }
+}
+
--- bus_messages/static/js/components/Container/Module/index.js
+++ bus_messages/static/js/components/Container/Module/index.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
-import ReactSpeedometer from "react-d3-speedometer"
+
+import GaugeSet from './GaugeSet'
 
 export default class Module extends React.Component {
   render () {
@@ -16,44 +17,7 @@ export default class Module extends React.Component {
           <Typography component="h4" variant="h4" gutterBottom>
             {data.serial_no.substr(data.serial_no.length - 5)}
           </Typography>
-          <Paper className={classes.moduleSection}>
-            <Grid container item xs={12} spacing={16}>
-              <Grid item xs={4}>
-                <ReactSpeedometer
-                  width={120}
-                  height={90}
-                  ringWidth={20}
-                  minValue={0}
-                  maxValue={50}
-                  startColor="blue"
-                  endColor="purple"
-                  value={data.avg_cell_temperature}
-                  currentValueText="Temp: ${value}º C"
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <ReactSpeedometer
-                  width={120}
-                  height={90}
-                  ringWidth={20}
-                  minValue={1.8}
-                  maxValue={6.0}
-                  value={data.avg_cell_voltage / 1000}
-                  currentValueText="Voltage: ${value} V"
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <ReactSpeedometer
-                  width={120}
-                  height={90}
-                  ringWidth={20}
-                  maxValue={10.0}
-                  value={data.amperes}
-                  currentValueText="Amperes: ${value}"
-                />
-              </Grid>
-            </Grid>
-          </Paper>
+          <GaugeSet classes={classes} data={data} />
         </Paper>
       </Grid>
     )
